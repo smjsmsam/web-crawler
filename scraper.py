1ef950932a78cce240d1b1c4258d5cf1f6e6f7a1
smjsmsam <smjsmsam@gmail.com>
2025-11-01 05:04:35 -0700

    add report
    Co-authored-by: kywx <kywx@users.noreply.github.com>

--- scraper.py
+++ scraper.py
@@ -1,22 +1,55 @@
 import re
-from urllib.parse import urlparse
-import lxml
+from utils import get_logger
+from urllib.parse import urlparse, urldefrag
+from lxml import html
 
+LOGGER = get_logger("SCRAPER")
 VISITED = set()
 SIMHASHES = list()
 SUBDOMAINS = dict()
 WORD_FREQ = dict()
 LONGEST_PAGE = ["", 0]
 BLACKLIST = set()
+CURRENT_LINKS = dict()
 
-DOMAINS = [".ics.uci.edu/",
-           ".cs.uci.edu/",
-           ".informatics.uci.edu/",
-           ".stat.uci.edu/"]
+REPORT_FILENAME = "report.txt"
+LINKS_PARSED = 0
+
+DOMAINS = ["ics.uci.edu",
+           "cs.uci.edu",
+           "informatics.uci.edu",
+           "stat.uci.edu"]
+
+STOP_WORDS = ["a", "about", "above", "after", "again", "against", "all",
+            "am", "an", "and", "any", "are", "aren", "t", "as", "at",
+            "be", "because", "been", "before", "being", "below", "between",
+            "both", "but", "by", "can", "not", "cannot", "could",
+            "couldn", "did", "didn", "do", "does", "doesn", "doing",
+            "don", "down", "during", "each", "few", "for", "from",
+            "further", "had", "hadn", "has", "hasn", "have", "haven",
+            "having", "he", "d", "ll", "s", "her", "here", "hers",
+            "herself", "him", "himself", "his", "how", "i", "if", "in",
+            "into", "is", "isn", "it", "its", "itself", "let", "me",
+            "more", "most", "mustn", "my", "myself", "no", "nor", "of",
+            "off", "on", "once", "only", "or", "other", "ought",
+            "our", "ours", "ourselves", "out", "over", "own", "same",
+            "shan", "she", "should", "shouldn", "so", "some", "such",
+            "than", "that", "the", "their", "theirs", "them", "themselves",
+            "then", "there", "these", "they", "this", "those",
+            "through", "to", "too", "under", "until", "up", "very",
+            "was", "wasn", "we", "what", "when", "where", "which",
+            "while", "who", "whom", "why", "with", "won", "would",
+            "wouldn", "you", "your", "yours", "yourself", "yourselves"]
+
 
 def scraper(url, resp):
+    get_logger("").error("HELLO????")
+    global LOGGER
+    LOGGER.error("HELLO????")
+    print("hello???")
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    return [urldefrag(link)[0] for link in links if is_valid(link)]
+
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -28,51 +61,116 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    global VISITED, BLACKLIST
-    # TODO:
-        # track longest page based on number of words
-        # generate total (across domains) list of common words ordered by frequency
-    if(url in VISITED or url in BLACKLIST or resp.status != 200):
+    global VISITED, BLACKLIST, LONGEST_PAGE, STOP_WORDS, WORD_FREQ,  \
+        REPORT_FILENAME, LINKS_PARSED, SUBDOMAINS, CURRENT_LINKS, LOGGER
+    
+    #TODO: politeness
+    if(urldefrag(url)[0] in VISITED or url in BLACKLIST or resp.status != 200):
+        LOGGER.info(resp.status + " status at " + resp.url + " : " + resp.error)
         print(resp.status + " status at " + resp.url + " : " + resp.error)
         return list()
-    
+    VISITED.append(urldefrag(url)[0])
+    LOGGER.info(url)
+    print(url)
+    #TODO: avoid crawling large files
+    # parse with lxml
     content = resp.raw_response.content
-    tree = lxml.html.fromstring(content)
-    links = tree.xpath('//a/@href')
-
-    # TODO:
-        # discard fragment part of url
-        # track subdomains of uci.edu and number of unique pages in each
-            # listed alphabetically
-            # format: subdomain, number
-    result = list()
-    for link in links:
-        print(link)
-        if is_valid(link):
-            result.append(link)
+    byte_count = len(content)
     
-    return result
+    tree = html.fromstring(content)
+
+    #TODO: simhash
+    # track longest page based on number of words
+    text_content = tree.text_content()
+    words = text_content.split()
+    word_count = len(words)
+    if word_count == 0:
+        return list()
+    if word_count > LONGEST_PAGE[1]:
+        LONGEST_PAGE = [url, word_count]
+    LOGGER.info(word_count)
+    print(word_count)
+    # generate total (across domains) list of common words ordered by frequency
+    for word in words:
+        if word not in STOP_WORDS:
+            val = WORD_FREQ.get(word)
+            if val == None:
+                WORD_FREQ[word] = 1
+            else:
+                WORD_FREQ[word] += 1    
+    
+    # write report every 10 links parsed
+    if LINKS_PARSED % 10 == 0:
+        with open(REPORT_FILENAME, 'w') as f:
+            f.write(f"Unique Pages: {len(VISITED)}\n")
+            f.write("-----------\n")
+            f.write(f"Longest Page: {LONGEST_PAGE[0]} with {LONGEST_PAGE[1]} words\n")
+            f.write("-----------\n")
+            f.write(f"Subdomains: {len(SUBDOMAINS)}\n")
+            f.write("-----------\n")
+            f.write("List of Subdomains:\n\n")
+            subdomain_freq = sorted_alphabetical(SUBDOMAINS)
+            for key in subdomain_freq.keys():
+                f.write(f"{key}, {subdomain_freq.get(key)}\n")
+            f.write("-----------\n")
+            f.write(f"50 Most Common Words:\n\n")
+            WORD_FREQ = sorted_frequency(WORD_FREQ)
+            for key in WORD_FREQ.keys():
+                f.write(f"{key}\n")
+
+    # get links
+    links = tree.xpath('//a/@href')
+    LOGGER.info(links)
+    print(links)
+    # count link frequency
+    CURRENT_LINKS.clear()
+    for link in links:
+        val = CURRENT_LINKS.get(link)
+        if val == None:
+            CURRENT_LINKS[link] = 1
+        else:
+            CURRENT_LINKS[link] += 1
+
+    return links
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-    global DOMAINS
+    global DOMAINS, SUBDOMAINS
     
     try:
-        parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"] or parsed.hostname not in DOMAINS):
+        parsed = urlparse(url, allow_fragments=False)
+        if parsed.scheme not in set(["http", "https"]) or \
+            re.match(
+                r".*\.(css|js|bmp|gif|jpe?g|ico"
+                + r"|png|tiff?|mid|mp2|mp3|mp4"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                + r"|epub|dll|cnf|tgz|sha1"
+                + r"|thmx|mso|arff|rtf|jar|csv"
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()) \
+            or not any(parsed.hostname in domain for domain in DOMAINS):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        
+        # track subdomains of uci.edu and number of unique pages in each
+        if parsed.hostname not in DOMAINS:
+            if not SUBDOMAINS[parsed.hostname]:
+                SUBDOMAINS[parsed.hostname] = set()
+            SUBDOMAINS[parsed.hostname].add(urldefrag(url)[0])
+        return True
 
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+
+def sorted_frequency(dictionary):
+    return sorted(dictionary.items(), key=lambda item: item[1], reverse=True)
+
+
+def sorted_alphabetical(dictionary):
+    return {key: len(dictionary[key]) for key in sorted(dictionary)}
+    
--- test.py
+++ test.py
@@ -1,17 +1,55 @@
 import sys
+import re
 import requests
-import lxml
+from lxml import html, etree
+from urllib.parse import urlparse
 
 
-url = "https://www.ics.uci.edu"
+url = "https://ics.uci.edu/"
+
+DOMAINS = ["ics.uci.edu",
+           "cs.uci.edu",
+           "informatics.uci.edu",
+           "stat.uci.edu"]
+ 
+
+def is_valid(url):
+    # Decide whether to crawl this url or not. 
+    # If you decide to crawl it, return True; otherwise return False.
+    # There are already some conditions that return False.
+    
+    try:
+        parsed = urlparse(url)
+        # print(parsed.hostname)
+        if parsed.scheme not in set(["http", "https"]) or parsed.hostname not in DOMAINS:
+            return False
+        
+        return not re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+    except TypeError:
+        print ("TypeError for ", parsed)
+        raise
+
 
 if __name__ == "__main__":
     resp = requests.get(url)
-    if(resp.status != 200):
+    if(resp.status_code != 200):
         print(resp.status_code)
         sys.exit(1)
     print(resp.status_code)
 
-    tree = lxml.html.fromstring(resp.content)
+    # print(resp.content)
+    tree = html.fromstring(resp.content)
     links = tree.xpath('//a/@href')
-    print(links)
+    # print(links)
+    for link in links:
+        if is_valid(link):
+            print(link)
