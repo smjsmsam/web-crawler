e02acd42bed02933f5de7324031956c3cf6434f0
smjsmsam <smjsmsam@gmail.com>
2025-11-01 17:07:05 -0700

    kinda working

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from simhash import Simhash, SimhashIndex
 import json
 from utils import get_logger
 
+
 LOGGER = get_logger("SCRAPER")
 VISITED = set()
 HASH_INDEX = SimhashIndex({}, k=3)
@@ -14,16 +15,16 @@ WORD_FREQ = dict()
 LONGEST_PAGE = ["", 0]
 BLACKLIST = set()
 CURRENT_LINKS = dict()
-
-REPORT_FILENAME = "report.txt"
 LINKS_PARSED = 0
+
+
 
 DOMAINS = ["ics.uci.edu",
            "cs.uci.edu",
            "informatics.uci.edu",
            "stat.uci.edu"]
 
-STOP_WORDS = ["&", "a", "about", "above", "after", "again", "against", "all",
+STOP_WORDS = ["a", "about", "above", "after", "again", "against", "all",
             "am", "an", "and", "any", "are", "aren", "t", "as", "at",
             "be", "because", "been", "before", "being", "below", "between",
             "both", "but", "by", "can", "not", "cannot", "could",
@@ -55,7 +56,7 @@ def scraper(url, resp):
     for link in links:
         if is_valid(link) and link != url:
             valid_links.append(urldefrag(link)[0])
-    print(valid_links)
+    # print(valid_links)
     return valid_links
     # return [urldefrag(link)[0] for link in links if is_valid(link)]
 
@@ -71,48 +72,65 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     global VISITED, BLACKLIST, LONGEST_PAGE, STOP_WORDS, WORD_FREQ,  \
-        REPORT_FILENAME, LINKS_PARSED, SUBDOMAINS, CURRENT_LINKS, \
-        HASH_INDEX, LOGGER
+        LINKS_PARSED, SUBDOMAINS, CURRENT_LINKS, HASH_INDEX, LOGGER
     
 
     LOGGER.info("Extracting : " + urldefrag(url)[0])
+    no_visits = False
     try:
         with open("visited.txt", "r+") as f:
             if len(VISITED) == 0:
                 # load visited from backup file
+                no_visits = True
                 for line in f:
                     VISITED.add(line.rstrip())
-                try:
-                    # load longest page from backup file
-                    with open("longest-page.json", "r") as f:
-                        LONGEST_PAGE = json.load(f)
-                    # load word frequencies
-                    with open("word-frequencies.json", "r") as f:
-                        WORD_FREQ = json.load(f)
-                    # load subdomains
-                    with open("subdomains.json", 'r') as f:
-                        SUBDOMAINS = json.load(f)
-                    # load blacklist
-                    with open("blacklist.txt", 'r') as f:
-                        file_content = f.read().splitlines()
-                        BLACKLIST =  set(file_content)
-                    # load simhashes
-                    with open("simhashes.json", "r") as f:
-                        simhashes = dict(json.load(f))
-                        HASH_INDEX = SimhashIndex([(url, Simhash(text_content)) for url, text_content in simhashes])
-                except FileNotFoundError:
-                    pass
             print(VISITED)
             if(urldefrag(url)[0] in VISITED or url in BLACKLIST or resp.status != 200):
-                LOGGER.debug(str(resp.status) + " status at " + resp.url + " : " + resp.error)
+                LOGGER.debug(f"{resp.status} status at {resp.url} : {resp.error}")
                 return list()
-            VISITED.add(urldefrag(url)[0])
             f.write(urldefrag(url)[0] + "\n")            
     except FileNotFoundError:
-        pass
+        with open('visited.txt', 'w') as f:
+            print("Created visited.txt")
+            f.write(urldefrag(url)[0] + "\n")
+    VISITED.add(urldefrag(url)[0])
+
+    if no_visits:
+        # load (if restarted)
+        try:
+            # load longest page from backup file
+            with open("longest-page.json", "r") as f1:
+                LONGEST_PAGE = json.load(f1)
+            # load word frequencies
+            with open("word-frequencies.json", "r") as f2:
+                WORD_FREQ = json.load(f2)
+            # load subdomains
+            with open("subdomains.json", 'r') as f3:
+                raw_dict = json.load(f3)
+                for subdomain, unique_pages in raw_dict.items():
+                    SUBDOMAINS[subdomain] = set(unique_pages)
+            # load blacklist
+            with open("blacklist.txt", 'r') as f4:
+                file_content = f4.read().splitlines()
+                BLACKLIST =  set(file_content)
+        except FileNotFoundError:
+            pass
     
+        try:
+            # load simhashes
+            with open("simhashes.json", "r") as f5:
+                simhashes = dict(json.load(f5))
+                HASH_INDEX = SimhashIndex([(url, Simhash(text_content)) for url, text_content in simhashes])
+        except FileNotFoundError:
+            pass
+
     # avoid crawling large files
-    content = resp.raw_response.content
+    try:
+        content = resp.raw_response.content
+    except Exception as e:
+        LOGGER.debug(f"No content from page: {url}; Exception: {e}")
+        print(f"No content from page: {url}; Exception: {e}")
+        return list()
     byte_count = len(content)
     if byte_count > 10000000:
         # discord says 10MB is a lot
@@ -127,10 +145,11 @@ def extract_next_links(url, resp):
     text_content = tree.text_content()
     current_hash = Simhash(text_content)
     if HASH_INDEX.get_near_dups(current_hash):
+        HASH_INDEX.add(url, current_hash)
         return list()
     HASH_INDEX.add(url, current_hash)
     with open("simhashes.json", "w") as f:
-        json.dump({urldefrag(url)[0]: current_hash})
+        json.dump({urldefrag(url)[0]: current_hash.value}, f)
     
     # track longest page based on number of words
     words = text_content.split()
@@ -146,7 +165,8 @@ def extract_next_links(url, resp):
     
     # generate total (across domains) list of common words ordered by frequency
     for word in words:
-        if word not in STOP_WORDS:
+        word = word.lower()
+        if word not in STOP_WORDS and word.isalnum():
             val = WORD_FREQ.get(word)
             if val == None:
                 WORD_FREQ[word] = 1
@@ -155,42 +175,11 @@ def extract_next_links(url, resp):
     
     # write report every n links parsed
     if LINKS_PARSED % 1 == 0:
-        with open(REPORT_FILENAME, 'w') as f:
-            f.write(f"Unique Pages: {len(VISITED)}\n")
-            f.write("-----------\n")
-            f.write(f"Longest Page: {LONGEST_PAGE[0]} with {LONGEST_PAGE[1]} words\n")
-            f.write("-----------\n")
-            f.write(f"Subdomains: {len(SUBDOMAINS)}\n")
-            f.write("-----------\n")
-            f.write("List of Subdomains:\n\n")
-            subdomain_freq = sorted_alphabetical(SUBDOMAINS)
-            for key in subdomain_freq.keys():
-                f.write(f"{key}, {subdomain_freq.get(key)}\n")
-            f.write("-----------\n")
-            f.write(f"50 Most Common Words:\n\n")
-            WORD_FREQ = sorted_frequency(WORD_FREQ)
-            for key in list(WORD_FREQ.keys())[:50]:
-                f.write(f"{key}\n")
-        with open("longest-page.json", 'w') as f1, \
-            open("word-frequencies.json", 'w') as f2, \
-            open("subdomains.json", 'w') as f3:
-            
-            json.dump(LONGEST_PAGE, f1)
-            json.dump(WORD_FREQ, f2)
-            json.dump(SUBDOMAINS, f3)
+        write_report()
     LINKS_PARSED += 1
 
     # get links
     links = set([urljoin(url, link) for link in tree.xpath('//a/@href')])
-
-    # count link frequency
-    # CURRENT_LINKS.clear()
-    # for link in links:
-    #     val = CURRENT_LINKS.get(link)
-    #     if val == None:
-    #         CURRENT_LINKS[link] = 1
-    #     else:
-    #         CURRENT_LINKS[link] += 1
 
     return list(links)
 
@@ -205,14 +194,16 @@ def is_valid(url):
         parsed = urlparse(url, allow_fragments=False)
         if parsed.scheme not in set(["http", "https"]) or \
             re.match(
-                r".*\.(css|js|bmp|gif|jpe?g|ico"
+                r".*\.(css|js|bmp|gif|img|jpe?g|ico"
                 + r"|png|tiff?|mid|mp2|mp3|mp4"
                 + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
                 + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-                + r"|epub|dll|cnf|tgz|sha1"
+                + r"|epub|dll|cnf|tgz|sha1|docs|docx"
                 + r"|thmx|mso|arff|rtf|jar|csv"
-                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()) \
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz|htm|css|js"
+                + r"|calendar|events|event|date|blog|page|archive"
+                + ")$", parsed.path.lower()) \
             or not any(parsed.hostname and parsed.hostname.endswith(domain) for domain in DOMAINS):
             return False
         
@@ -234,4 +225,32 @@ def sorted_frequency(dictionary):
 
 def sorted_alphabetical(dictionary):
     return {key: len(dictionary[key]) for key in sorted(dictionary)}
+
+
+def write_report():
+    global VISITED, BLACKLIST, LONGEST_PAGE, WORD_FREQ,  \
+        LINKS_PARSED, SUBDOMAINS
+    with open("report.txt", 'w') as f:
+        f.write(f"Unique Pages: {len(VISITED)}\n")
+        f.write("-----------\n")
+        f.write(f"Longest Page: {LONGEST_PAGE[0]} with {LONGEST_PAGE[1]} words\n")
+        f.write("-----------\n")
+        f.write(f"Subdomains: {len(SUBDOMAINS)}\n")
+        f.write("-----------\n")
+        f.write("List of Subdomains:\n\n")
+        subdomain_freq = sorted_alphabetical(SUBDOMAINS)
+        for key in subdomain_freq.keys():
+            f.write(f"{key}, {subdomain_freq.get(key)}\n")
+        f.write("-----------\n")
+        f.write(f"50 Most Common Words:\n\n")
+        WORD_FREQ = sorted_frequency(WORD_FREQ)
+        for key in list(WORD_FREQ.keys())[:50]:
+            f.write(f"{key}\n")
     
+    with open("longest-page.json", 'w') as f1, \
+        open("word-frequencies.json", 'w') as f2, \
+        open("subdomains.json", 'w') as f3:
+        
+        json.dump(LONGEST_PAGE, f1)
+        json.dump(WORD_FREQ, f2)
+        json.dump(list(SUBDOMAINS), f3)
